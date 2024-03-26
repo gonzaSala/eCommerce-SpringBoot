4785b7fdf903257a092b28267f6989cd4fa78b92
gonzaSala <gonzasalamendi@gmail.com>
2024-03-26 12:19:53 -0300

    archivos ananidos

---
+++ src/test/java/com/ecommerceTest/SpringEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
