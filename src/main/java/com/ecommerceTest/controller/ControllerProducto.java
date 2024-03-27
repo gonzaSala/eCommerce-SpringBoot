3e33f7aad82e5b52958b7d2acd34ccedde6931fe
gonzaSala <gonzasalamendi@gmail.com>
2024-03-27 11:20:34 -0300

    metodo create prodcuto

--- src/main/java/com/ecommerceTest/controller/ControllerProducto.java
+++ src/main/java/com/ecommerceTest/controller/ControllerProducto.java
@@ -1,13 +1,18 @@
 package com.ecommerceTest.controller;
 
+import com.ecommerceTest.model.Producto;
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/productos")
 public class ControllerProducto {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ControllerProducto.class);
     @GetMapping("")
     public String show() {
         return "productos/show";
@@ -16,5 +21,11 @@ public class ControllerProducto {
     @GetMapping("/create")
     public String create(){
         return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("este es el objeto producto {}", producto);
+        return "redirect:/productos";
     }
 }
