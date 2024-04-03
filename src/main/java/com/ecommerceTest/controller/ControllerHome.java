c42b34bf059d8c4478cd31c5ccaf836fe0319575
gonzaSala <gonzasalamendi@gmail.com>
2024-04-02 22:05:05 -0300

    add carrito

--- src/main/java/com/ecommerceTest/controller/ControllerHome.java
+++ src/main/java/com/ecommerceTest/controller/ControllerHome.java
@@ -1,5 +1,7 @@
 package com.ecommerceTest.controller;
 
+import com.ecommerceTest.model.DetalleOrden;
+import com.ecommerceTest.model.Orden;
 import com.ecommerceTest.model.Producto;
 import com.ecommerceTest.service.ServiceProducto;
 import org.slf4j.Logger;
@@ -7,10 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -21,24 +24,59 @@ public class ControllerHome {
     @Autowired
     private ServiceProducto serviceProducto;
 
+    List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+    Orden orden = new Orden();
+
 
     @GetMapping("")
-    public String home(Model model){
+    public String home(Model model) {
 
         model.addAttribute("productos", serviceProducto.findAll());
         return "usuario/home";
     }
 
     @GetMapping("producto_home/{id}")
-    public String productoHome(@PathVariable int id, Model model){
+    public String productoHome(@PathVariable int id, Model model) {
         log.info("Id enviado como parámetro: {}", id);
 
         Producto producto = new Producto();
         Optional<Producto> productoOptional = serviceProducto.get(id);
         producto = productoOptional.get();
 
-        model.addAttribute("producto",producto);
+        model.addAttribute("producto", producto);
         return "usuario/producto_home";
+    }
+
+    @PostMapping("/cart")
+    public String addCart(@RequestParam int id , @RequestParam int cantidad, Model model) {
+
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+
+        Optional<Producto> optionalProducto = serviceProducto.get(id);
+        log.info("producto añanididdo: {}" , optionalProducto.get());
+        log.info("Cantidad: {}", cantidad );
+        log.info("ID: {}", id );
+
+        producto=optionalProducto.get();
+
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(producto.getPrecio());
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setTotal(producto.getPrecio()*cantidad);
+        detalleOrden.setProducto(producto);
+
+        detalles.add(detalleOrden);
+
+        sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+        return "usuario/carrito";
     }
 
 }
--- src/main/java/com/ecommerceTest/model/Producto.java
+++ src/main/java/com/ecommerceTest/model/Producto.java
@@ -85,4 +85,16 @@ public class Producto {
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", imagen='" + imagen + '\'' +
+                ", precio=" + precio +
+                ", cantidad=" + cantidad +
+                '}';
+    }
 }
